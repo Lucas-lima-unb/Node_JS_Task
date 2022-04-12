377d5c21a2b5816a1b5b0c692faf138cca447ed0
LUCAS FERREIRA LIMA <slashl@hotmail.com>
2022-04-12 17:31:46 -0300

    Mongodb update
    Created users and routes

--- wweb-app/routes/index.js
+++ wweb-app/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res) => {
+  const db = require("../db");
+  const Users = db.Mongoose.model('users', db.UserSchema, 'users');
+ 
+  const docs = await Users.find({}).lean().exec();
+  res.render('index', { docs });
 });
-
 module.exports = router;
